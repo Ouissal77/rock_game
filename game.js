05b6d73b0bcd5323c6de0fed51cba388be990e5b
Ouissal EL HAMAMI <ouissal.elhamami@gmail.com>
2023-02-03 14:10:02 +0100

    experiencing branches

--- game.js
+++ game.js
@@ -9,31 +9,39 @@ function getComputerChoice() {
 function round(computer,player) 
 {
     player=player.charAt().toUpperCase() + player.slice(1).toLowerCase();
-    if (player === computer) {
-        alert("it's a tie darlin")
-        return ("it's a tie !! try again");
-     }
-    else if ((player === "Rock" && computer === "Paper") || 
-             (player === "Paper" && computer === "Scissors") || 
-             (player === "Scissors" && computer === "Rock")) 
-                {
-                    console.log(("You lose !" + player + " defeat " + computer))
-                    alert(("You lose !" + player + " defeat " + computer))
-                 return 0;
-                }
+    if (player ==="Rock" || player ==="Paper" || player ==="Scissors")
+    {
+        if (player === computer) {
+            alert("it's a tie darlin")
+            return ("it's a tie !! try again");
+        }
+        else if ((player === "Rock" && computer === "Paper") || 
+                (player === "Paper" && computer === "Scissors") || 
+                (player === "Scissors" && computer === "Rock")) 
+                    {
+                        console.log(("You lose !" + player + " defeat " + computer))
+                        alert(("You lose !" + player + " defeat " + computer))
+                    return 0;
+                    }
+        else {
+            console.log("You win! " + player + " defeat " + computer);
+            alert("You win! " + player + " defeat " + computer);
+            return 1;}
+    }
     else {
-        console.log("You win! " + player + " defeat " + computer);
-        alert("You win! " + player + " defeat " + computer);
-         return 1;}
+        alert("please enter a proper choice");
+    return ("not valid")}
 
 }
    
 
-function game(){
+function game()
+{
     let m=0,k=0;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
         
-        player=prompt("what's your play now! ");
+        player = prompt("what's your play now! ");
+       
         z=round(getComputerChoice(),player);
         console.log(z);
        
@@ -41,8 +49,9 @@ function game(){
         else if (z===1) k=k+1;
 
     }
-if (m>k) {console.log("you lost this game"); alert("you lost this game");}
-else if (m<k) {console.log("You Are a winner of this Round"); alert("You Are a winner of this Round");}
-else {console.log("no one wins lol"); alert("no one wins lol!!!");}
+    if (m>k) {console.log("you lost this game"); alert("you lost this game");}
+    else if (m<k) {console.log("You Are a winner of this Round"); alert("You Are a winner of this Round");}
+    else {console.log("no one wins lol"); alert("no one wins lol!!!");}
 }
+
 game();
